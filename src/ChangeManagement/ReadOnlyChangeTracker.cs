8beefab7a73445cca97ce825c56e228fe69e9bbb
Frans Bouma <frans@sd.nl>
2014-12-29 12:30:51 +0100

    Moved the change tracker implementations to their own file
    They were private classes but their implementation is significant.
    They're now internal instead of private so the parent class isn't used
    as a private namespace.

---
+++ src/ChangeManagement/ReadOnlyChangeTracker.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace System.Data.Linq
+{
+	using System.Data.Linq.Mapping;
+	using System.Data.Linq.Provider;
+
+	/// <summary>
+	/// This is the implementation used when change tracking is disabled.
+	/// </summary>
+	internal class ReadOnlyChangeTracker : ChangeTracker
+	{
+		internal override TrackedObject Track(object obj) { return null; }
+		internal override TrackedObject Track(object obj, bool recurse) { return null; }
+		internal override void FastTrack(object obj) 
+		{ 
+			// nop
+		}
+		internal override bool IsTracked(object obj) { return false; }
+		internal override TrackedObject GetTrackedObject(object obj) { return null; }
+		internal override void StopTracking(object obj) { }
+		internal override void AcceptChanges() 
+		{ 
+			// nop
+		}
+		internal override IEnumerable<TrackedObject> GetInterestingObjects() { return new TrackedObject[0]; }
+	}
+}
+
